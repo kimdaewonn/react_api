d5ae0545a9b1b120a6f7b16bd60e06d7ec261f8e
unknown <eodnjs9605@gmail.com>
2022-11-03 20:45:33 +0900

    1103

--- src/components/pages/Movie.jsx
+++ src/components/pages/Movie.jsx
@@ -6,6 +6,7 @@ import Footer from "../layout/Footer";
 import Title from "../layout/Title";
 import MovieCont from "../include/MovieCont";
 import Contact from "../layout/Contact";
+
 const Movie = () => {
   const [movies, setMovies] = useState([]);
   useEffect(() => {
